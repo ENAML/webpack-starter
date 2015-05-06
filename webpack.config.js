d1f2b028c8d5dd24a3b524f9e0ecc21534032ec6
enaml <monotonalist@gmail.com>
2015-05-06 16:27:56 -0400

    build command setup

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ var config = {
     vendors: ['jQuery']
   },
   output: {
-    path: __dirname,
+    path: process.env.NODE_ENV === 'production' ? './dist' : __dirname,
     filename: "bundle.js"
   },
   module: {
